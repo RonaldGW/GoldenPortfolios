307d0cea7c326cca4d488ef6489c4d7e08171146
Ronald <ronald@big.red>
2021-10-13 14:37:26 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CareersComponent } from './careers/careers.component';
+import { ContactComponent } from './contact/contact.component';
+import { ExamplesComponent } from './examples/examples.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { ReportComponent } from './report/report.component';
+import { TeamComponent } from './team/team.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '/home', component: HomeComponent },
+  { path: '/team', component: TeamComponent },
+  { path: '/pricing', component: PricingComponent },
+  { path: '/examples', component: ExamplesComponent  },
+  { path: '/about', component:  AboutComponent},
+  { path: '/login', component:  LoginComponent},
+  { path: '/careers', component:  CareersComponent},
+  { path: '/report', component:  ReportComponent},
+  { path: '/contact', component:  ContactComponent},
+  { path: '', redirectTo: 'home',  pathMatch: 'full' },
+  { path: '**', redirectTo: 'home',  pathMatch: 'full' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MainNavbarComponent } from './main-navbar/main-navbar.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { ExamplesComponent } from './examples/examples.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { TeamComponent } from './team/team.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { ReportComponent } from './report/report.component';
+import { CareersComponent } from './careers/careers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavbarComponent,
+    PricingComponent,
+    ExamplesComponent,
+    AboutComponent,
+    LoginComponent,
+    HomeComponent,
+    TeamComponent,
+    FooterComponent,
+    ContactComponent,
+    ReportComponent,
+    CareersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
